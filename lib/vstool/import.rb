14efc4156f7bd4f4a0dc8f5d300bbb24684f6873
Yusuke Yachi <yyachi@misasa.okayama-u.ac.jp>
2019-10-31 10:41:05 +0900

    add option parser

--- lib/vstool/import.rb
+++ lib/vstool/import.rb
@@ -1,24 +1,37 @@
 module Vstool
     class Import < Base
-        def initialize(output, argv = ARGV)
-            @io = output
-            @io.puts "initializing..."
-            @argv = argv
+        def initialize(params = {})
+			@params = params
+			@output = params[:output] || STDOUT
+			@stderr = params[:stderr] || STDERR
+			@verbose = params[:verbose] || false
+
 			@option_parser = OptionParser.new do |opts|
 				opts.banner = "usage: vs-import [options] VS-DIR SURFACE-NAME"
 				opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
-			     	@verbose = v
+			     	@params[:verbose] = v
 			   	end
 			 	opts.on_tail("-h", "--help", "Show this message.") do |v|
-					STDERR.puts opts.to_s
+					@stderr.puts opts.to_s
 					exit
 				end
 			end            
-            @params = @option_parser.parse(argv)
         end
 
-        def cmd_options(argv = ARGV)
-            
+        def verbose
+            @params[:verbose]
+        end
+
+        def run(argv = ARGV)
+            argv = @option_parser.parse(argv)
+            @output.puts "argv: #{argv}" if verbose
+            @output.puts "options: #{@params}" if verbose
+            raise "invalid args" unless argv.size == 2
+            vs_dir = argv.shift
+            surface_name = argv.shift
+            @output.puts "VS-DIR: #{vs_dir}" if verbose
+            @output.puts "SURFACE-NAME #{surface_name}" if verbose
+            #@stderr.puts "not implemented"
         end
     end
 end
--- spec/vstool/import_spec.rb
+++ spec/vstool/import_spec.rb
@@ -2,16 +2,35 @@ require 'spec_helper'
 
 module Vstool
     describe Import do
-        describe "#new" do
+        describe "#new", :current => true do
+            subject { Import.new(params) }
             let(:output) { double('output').as_null_object }
             let(:params) { {:output => output}}
             let(:argv) { ["-h"] }
-            let(:app) { Import.new(output, argv) }
-            before do
-                app
+            let(:app) { Import.new(params) }
+            it "with output" do
+                expect{ subject }.not_to raise_error
             end
-            it "show usage and exit" do
-                STDERR.should_receive(:puts).with(/^usage:/)
+        end
+
+        describe "run", :current => true do
+            subject { app.run(argv) }
+            let(:output) { double('output') }
+            let(:params) { {:stderr => output}}
+            let(:app) { Import.new(params) }
+            context "with valid args" do
+                let(:argv) {["VS-DIR", "SURFACE-NAME"]}
+                it "show message" do
+                    allow(output).to receive(:puts).with("not implemented")
+                    subject
+                end
+            end
+            context "without args" do
+                let(:argv) {[]}
+                it "raise error" do
+                    #allow(output).to receive(:puts).with("not implemented")
+                    expect{ subject }.to raise_error
+                end
             end
         end
 
