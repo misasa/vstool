c67cdb2e164264bdd544f710f55854776dbb7321
Yusuke Yachi <yyachi@misasa.okayama-u.ac.jp>
2019-10-31 11:45:42 +0900

    add vs-checkout -commit -update commands

--- lib/vstool/medusa_sync.rb
+++ lib/vstool/medusa_sync.rb
@@ -12,7 +12,7 @@ module Vstool
         end
 
         def run_import(argv = ARGV)
-			@option_parser = OptionParser.new do |opts|
+			option_parser = OptionParser.new do |opts|
 				opts.banner = "usage: vs-import [options] VS-DIR SURFACE-NAME"
 				opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
 			     	@params[:verbose] = v
@@ -22,12 +22,12 @@ module Vstool
 					exit
 				end
 			end                        
-            argv = @option_parser.parse(argv)
+            argv = option_parser.parse(argv)
             @output.puts "argv: #{argv}" if verbose
             @output.puts "options: #{@params}" if verbose
             unless argv.size == 2
                 @stderr.puts "invalid arguments"
-                @stderr.puts @option_parser.banner
+                @stderr.puts option_parser.banner
                 exit
             end
             vs_dir = argv.shift
@@ -36,5 +36,80 @@ module Vstool
             @output.puts "SURFACE-NAME #{surface_name}" if verbose
             @stderr.puts "not implemented"
         end
+
+        def run_checkout(argv = ARGV)
+			option_parser = OptionParser.new do |opts|
+				opts.banner = "usage: vs-checkout [options] SURFACE-ID VS-DIR"
+				opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+			     	@params[:verbose] = v
+			   	end
+			 	opts.on_tail("-h", "--help", "Show this message.") do |v|
+					@stderr.puts opts.to_s
+					exit
+				end
+			end                        
+            argv = option_parser.parse(argv)
+            @output.puts "argv: #{argv}" if verbose
+            @output.puts "options: #{@params}" if verbose
+            unless argv.size == 2
+                @stderr.puts "invalid arguments"
+                @stderr.puts option_parser.banner
+                exit
+            end
+            surface_id = argv.shift
+            vs_dir = argv.shift
+            @output.puts "SURFACE-ID: #{surface_id}" if verbose
+            @output.puts "VS-DIR #{vs_dir}" if verbose
+            @stderr.puts "not implemented"
+        end
+
+        def run_update(argv = ARGV)
+			option_parser = OptionParser.new do |opts|
+				opts.banner = "usage: vs-update [options] VS-DIR"
+				opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+			     	@params[:verbose] = v
+			   	end
+			 	opts.on_tail("-h", "--help", "Show this message.") do |v|
+					@stderr.puts opts.to_s
+					exit
+				end
+			end                        
+            argv = option_parser.parse(argv)
+            @output.puts "argv: #{argv}" if verbose
+            @output.puts "options: #{@params}" if verbose
+            unless argv.size == 1
+                @stderr.puts "invalid arguments"
+                @stderr.puts option_parser.banner
+                exit
+            end
+            vs_dir = argv.shift
+            @output.puts "VS-DIR #{vs_dir}" if verbose
+            @stderr.puts "not implemented"
+        end
+
+        def run_commit(argv = ARGV)
+			option_parser = OptionParser.new do |opts|
+				opts.banner = "usage: vs-commit [options] VS-DIR"
+				opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+			     	@params[:verbose] = v
+			   	end
+			 	opts.on_tail("-h", "--help", "Show this message.") do |v|
+					@stderr.puts opts.to_s
+					exit
+				end
+			end                        
+            argv = option_parser.parse(argv)
+            @output.puts "argv: #{argv}" if verbose
+            @output.puts "options: #{@params}" if verbose
+            unless argv.size == 1
+                @stderr.puts "invalid arguments"
+                @stderr.puts option_parser.banner
+                exit
+            end
+            vs_dir = argv.shift
+            @output.puts "VS-DIR #{vs_dir}" if verbose
+            @stderr.puts "not implemented"
+        end
+
     end
 end
--- spec/vstool/medusa_sync_spec.rb
+++ spec/vstool/medusa_sync_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 module Vstool
     describe MedusaSync do
-        describe "#new", :current => true do
+        describe "#new", :current => false do
             subject { MedusaSync.new(params) }
             let(:output) { double('output').as_null_object }
             let(:params) { {:output => output}}
@@ -12,11 +12,18 @@ module Vstool
             end
         end
 
-        describe "run_import", :current => true do
+        describe "run_import", :current => false do
             subject { app.run_import(argv) }
             let(:output) { double('output') }
             let(:params) { {:stderr => output}}
             let(:app) { MedusaSync.new(params) }
+            context "with -h" do
+                let(:argv) {["-h"]}
+                let(:params){ {} }
+                it "show banner" do
+                    expect{ subject }.to raise_error SystemExit
+                end
+            end
             context "with valid args" do
                 let(:argv) {["VS-DIR", "SURFACE-NAME"]}
                 it "show message" do
@@ -32,6 +39,87 @@ module Vstool
                 end
             end
         end
+
+        describe "run_checkout", :current => true do
+            subject { app.run_checkout(argv) }
+            let(:output) { double('output') }
+            let(:params) { {:stderr => output}}
+            let(:app) { MedusaSync.new(params) }
+            context "with -h" do
+                let(:argv) {["-h"]}
+                let(:params){ {} }
+                it "show banner" do
+                    expect{ subject }.to raise_error SystemExit
+                end
+            end
+            context "with valid args" do
+                let(:argv) {["-v", "surface_id", "vs_dir"]}
+                it "show message" do
+                    allow(output).to receive(:puts).with("not implemented")
+                    subject
+                end
+            end
+            context "without args" do
+                let(:argv) {[]}
+                it "raise error" do
+                    expect{ subject }.to raise_error
+                end
+            end
+        end        
+
+        describe "run_update", :current => true do
+            subject { app.run_update(argv) }
+            let(:output) { double('output') }
+            let(:params) { {:stderr => output}}
+            let(:app) { MedusaSync.new(params) }
+            context "with -h" do
+                let(:argv) {["-h"]}
+                let(:params){ {} }
+                it "show banner" do
+                    expect{ subject }.to raise_error SystemExit
+                end
+            end
+            context "with valid args" do
+                let(:argv) {["-v", "vs_dir"]}
+                it "show message" do
+                    allow(output).to receive(:puts).with("not implemented")
+                    subject
+                end
+            end
+            context "without args" do
+                let(:argv) {[]}
+                it "raise error" do
+                    expect{ subject }.to raise_error
+                end
+            end
+        end                
+
+        describe "run_commit", :current => true do
+            subject { app.run_commit(argv) }
+            let(:output) { double('output') }
+            let(:params) { {:stderr => output}}
+            let(:app) { MedusaSync.new(params) }
+            context "with -h" do
+                let(:argv) {["-h"]}
+                let(:params){ {} }
+                it "show banner" do
+                    expect{ subject }.to raise_error SystemExit
+                end
+            end
+            context "with valid args" do
+                let(:argv) {["-v", "vs_dir"]}
+                it "show message" do
+                    allow(output).to receive(:puts).with("not implemented")
+                    subject
+                end
+            end
+            context "without args" do
+                let(:argv) {[]}
+                it "raise error" do
+                    expect{ subject }.to raise_error
+                end
+            end
+        end                
 
     end
 end
